2f90952eaec585641554003920f031114de9dedc
Houari Aouinti <169095690+aouintihouari@users.noreply.github.com>
2026-01-01 01:32:51 +0100

    chore(frontend): initialize project with vite, ts paths and tailwind v4

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold underline">Setup OK</h1>
+    </div>
+  );
+}
+export default App;
